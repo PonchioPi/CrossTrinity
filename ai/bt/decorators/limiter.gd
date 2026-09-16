137ce180d0e4d16144d8b52334673bab436a6ba9
PonchioPi <163079128+PonchioPi@users.noreply.github.com>
2026-09-16 20:28:49 +0200

    Create limiter.gd

---
+++ ai/bt/decorators/limiter.gd
@@ -0,0 +1,22 @@
+class_name BTLimiter
+extends BTDecorator
+
+@export_range(0, 100, 1, "or_greater") var count_limit:int
+
+func _init() -> void:
+    cache_key = "Limiter#%s"%[self.get_instance_id()]
+
+func tick(actor:Node, blackboard:BlackBoard) -> void:
+    var count:int = blackboard._get_("count", 0, cache_key)
+    if count < count_limit:
+        status = branches[0]._tick(actor, blackboard)
+        if is_standby():
+            blackboard._set_("count", count + 1, cache_key)
+        return
+    else:
+        status = 0b100
+        return
+
+func reset(blackboard:BlackBoard) -> void:
+    blackboard.erase("count", cache_key)
+        
