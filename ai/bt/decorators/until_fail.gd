430c3c4baff8ced0c17218677c665ff3e47121d0
PonchioPi <163079128+PonchioPi@users.noreply.github.com>
2026-09-16 20:49:21 +0200

    Create until_fail.gd

---
+++ ai/bt/decorators/until_fail.gd
@@ -0,0 +1,13 @@
+class_name BTUntilFail
+extends BTDecorator
+
+func _init() -> void:
+    cache_key = "UntilSuccess#%s"%[self.get_instance_id()]
+
+func tick(actor:Node, blackboard:BlackBoard) -> void:
+    status = branches[0]._tick(actor, blackboard)
+    if (status & 0b11) == 0:
+        status = 0b101
+        return
+    else:
+        status = 0b110
