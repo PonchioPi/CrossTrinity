18fd40bf365c223a900c0e2231327b95f4fda4b7
PonchioPi <163079128+PonchioPi@users.noreply.github.com>
2026-09-16 20:07:42 +0200

    Create delayer.gd

---
+++ ai/bt/decorators/delayer.gd
@@ -0,0 +1,19 @@
+class_name BTDelayer
+extends BTDecorator
+
+@export_range(0.0, 3600, 0.001, "or_greater") var delay:float
+
+func _init() -> void:
+    cache_key = "Delayer#%s"%[self.get_instance_id()]
+
+func tick(actor:Node, blackboard:BlackBoard) -> void:
+    var remaining:float = blackboard._get_("remaining_time", 0.0, cache_key)
+    if remaining < delay:
+        remaining += tree.get_delta()
+        blackboard._set_("remaining_time", remaining, cache_key)
+        status = 0b110
+    else:
+        status = branches[0]._tick(actor, blackboard)
+        if is_standby():
+            blackboard.erase("remaining_time", cache_key)
+    return
