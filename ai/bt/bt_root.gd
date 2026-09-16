77c1a32359fd659026bd62275f9a6cf54915b14a
PonchioPi <163079128+PonchioPi@users.noreply.github.com>
2026-09-16 06:22:58 +0200

    Create bt_root.gd

---
+++ ai/bt/bt_root.gd
@@ -0,0 +1,17 @@
+class_name BTRoot
+extends BTTask
+
+func _init() -> void:
+    cache_key = "Root#%s"%[self.get_instance_id()]
+
+func set_branches(value:Array[BTTask]) -> void:
+    branches = value
+    if branches.size() != 1:
+        push_error("Behavior Tree error: %s should have only one child (Tree: %s)"%[cache_key, tree.name])
+        return
+    branches[0].rank = rank + 1
+    branches[0].set_tree(tree)
+    branches_changed.emit(self.get_instance_id(), branches)
+
+func tick(actor:Node, blackboard:Blackboard) -> void:
+    status = ((status & 0b100) | branches[0]._tick(actor, blackboard))
