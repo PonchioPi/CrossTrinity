4a2ec4f8f02db80151419d7953b53533d425585a
PonchioPi <163079128+PonchioPi@users.noreply.github.com>
2026-09-16 20:12:51 +0200

    Create inverter.gd

---
+++ ai/bt/decorators/inverter.gd
@@ -0,0 +1,12 @@
+class_name BTInverter
+extends BTDecorator
+
+func _init() -> void:
+    cache_key = "Inverter#%s"%[self.get_instance_id()]
+
+func tick(actor:Node, blackboard:BlackBoard) -> void:
+    status = branches[0]._tick(actor, blackboard)
+    if not is_standby():
+        return
+    else:
+        status = (status ^ 0b1)
