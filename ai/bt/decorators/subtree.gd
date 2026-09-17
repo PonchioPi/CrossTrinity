f34a43b1e7c3037a042c5377a51d370ef6063402
PonchioPi <163079128+PonchioPi@users.noreply.github.com>
2026-09-17 05:35:59 +0200

    Update subtree.gd

--- ai/bt/decorators/subtree.gd
+++ ai/bt/decorators/subtree.gd
@@ -41,4 +41,4 @@ func init_tree(tree_node:Object) -> void:
     behavior_subtree_changed.emit()
 
 func tick(actor:Node, blackboard:BlackBoard) -> void:
-    status = branches[0]._tick(actor, blackboard)
+    status = ((status & (~0b11)) | branches[0]._tick(actor, blackboard))
