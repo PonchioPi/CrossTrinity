74a1d6a2087a002480cb59ce86abc15790ec0c03
PonchioPi <163079128+PonchioPi@users.noreply.github.com>
2026-09-17 05:44:26 +0200

    Update until_success.gd
    Addition of a bit of DRY.

--- ai/bt/decorators/until_success.gd
+++ ai/bt/decorators/until_success.gd
@@ -5,7 +5,7 @@ func _init() -> void:
     cache_key = "UntilSuccess#%s"%[self.get_instance_id()]
 
 func tick(actor:Node, blackboard:BlackBoard) -> void:
-    status = ((status & (~0b11)) | branches[0]._tick(actor, blackboard))
+    super.tick(actor, blackboard)
     if (status & 0b11) == 1:
         status = 0b101
         return
