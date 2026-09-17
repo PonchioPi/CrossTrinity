a77fe64d901caebf72f304c595386913fba22ab7
PonchioPi <163079128+PonchioPi@users.noreply.github.com>
2026-09-17 02:44:33 +0200

    Update parallel.gd

--- ai/bt/composites/parallel.gd
+++ ai/bt/composites/parallel.gd
@@ -19,16 +19,37 @@ func _init() -> void:
 
 func init_tree(tree_node:Object) -> void:
     results.resize(branches.size())
-    for i in range(branches.size()):
-        child_order.append(i)
+    child_order = branches.duplicate()
     super.init_tree(tree_node)
 
 func _reset() -> void:
     results.clear()
     results.resize(branches.size())
     if random:
-        child_order = branches
+        child_order = branches.duplicate()
     super._reset()
 
 func tick(actor:Node, blackboard:BlackBoard) -> void:
-    
+    if is_random():
+        child_order.shuffle()
+    for index in branches.size():
+        status = child_order[index]._tick(actor, blackboard)
+        results[index] = status
+        blackboard._set_("status", status, cache_key)
+        if (status & 0b11) == 1:
+            if is_selector():
+                return
+        elif not is_standby():
+            continue
+        else:
+            if not is_selector():
+                return
+    if 0b110 in results or 0b111 in results:
+        status = 0b110
+        return
+    if not is_selector():
+        status = 0b101
+        return
+    else:
+        status = 0b100
+        return
